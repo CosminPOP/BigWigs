61a191abed6eef4831d8152148ae6f08bbec7f05
Cosmin POP <mtx.rake@gmail.com>
2021-03-29 15:11:45 +0300

    5 left to enrage bar enrage warning

--- Raids/Naxxramas/Maexxna.lua
+++ Raids/Naxxramas/Maexxna.lua
@@ -1,4 +1,3 @@
-
 ----------------------------------
 --      Module Declaration      --
 ----------------------------------
@@ -10,97 +9,101 @@ local module, L = BigWigs:ModuleDeclaration("Maexxna", "Naxxramas")
 --      Localization      --
 ----------------------------
 
-L:RegisterTranslations("enUS", function() return {
-	cmd = "Maexxna",
+L:RegisterTranslations("enUS", function()
+    return {
+        cmd = "Maexxna",
 
-	spray_cmd = "spray",
-	spray_name = "Web Spray Alert",
-	spray_desc = "Warn for webspray and spiders",
+        spray_cmd = "spray",
+        spray_name = "Web Spray Alert",
+        spray_desc = "Warn for webspray and spiders",
 
-	enrage_cmd = "enrage",
-	enrage_name = "Enrage Alert",
-	enrage_desc = "Warn for enrage",
+        enrage_cmd = "enrage",
+        enrage_name = "Enrage Alert",
+        enrage_desc = "Warn for enrage",
 
-	cocoon_cmd = "cocoon",
-	cocoon_name = "Cocoon Alert",
-	cocoon_desc = "Warn for Cocooned players",
+        cocoon_cmd = "cocoon",
+        cocoon_name = "Cocoon Alert",
+        cocoon_desc = "Warn for Cocooned players",
 
-	poison_cmd = "Poison",
-	poison_name = "Necrotic Poison Alert",
-	poison_desc = "Warn for Necrotic Poison",
+        poison_cmd = "Poison",
+        poison_name = "Necrotic Poison Alert",
+        poison_desc = "Warn for Necrotic Poison",
 
-	cocoontrigger = "(.*) (.*) afflicted by Web Wrap.",
-	webspraytrigger = "afflicted by Web Spray",
-	poisontrigger = "afflicted by Necrotic Poison.",
-	etrigger1 = "gains Enrage",
+        cocoontrigger = "(.*) (.*) afflicted by Web Wrap.",
+        webspraytrigger = "afflicted by Web Spray",
+        poisontrigger = "afflicted by Necrotic Poison.",
+        etrigger1 = "gains Enrage",
 
-	cocoonwarn = "%s Cocooned!",
-	poisonwarn = "Necrotic Poison!",
-	enragetrigger = "%s becomes enraged.",
+        cocoonwarn = "%s Cocooned!",
+        poisonwarn = "Necrotic Poison!",
+        enragetrigger = "%s becomes enraged.",
 
-	webspraywarn30sec = "Wall Cocoons in 10 seconds",
-	webspraywarn20sec = "Wall Cocoons! 15 seconds until Spiders spawn!",
-	webspraywarn10sec = "10 seconds until Web Spray!",
-	webspraywarn5sec = "AOE - Spiders Spawn - AOE! WEB SPRAY 5 SECONDS!",
-	webspraywarn = "Web Spray! 40 seconds until next!",
+        webspraywarn30sec = "Wall Cocoons in 10 seconds",
+        webspraywarn20sec = "Wall Cocoons! 15 seconds until Spiders spawn!",
+        webspraywarn10sec = "10 seconds until Web Spray!",
+        webspraywarn5sec = "AOE - Spiders Spawn - AOE! WEB SPRAY 5 SECONDS!",
+        webspraywarn = "Web Spray! 40 seconds until next!",
 
-	enragewarn = "Enrage - SQUISH SQUISH SQUISH!",
-	enragesoonwarn = "Enrage Soon - Bug Swatters out!",
+        enragewarn = "Enrage - SQUISH SQUISH SQUISH!",
+        enragesoonwarn = "Enrage Soon - Bug Swatters out!",
 
-	webspraybar = "Web Spray",
-	cocoonbar = "Cocoons",
-	spiderbar = "Spiders",
-	poisonbar = "Necrotic Poison",
+        webspraybar = "Web Spray",
+        cocoonbar = "Cocoons",
+        spiderbar = "Spiders",
+        poisonbar = "Necrotic Poison",
 
-	you = "You",
-	are = "are",
-} end )
+        you = "You",
+        are = "are",
+    }
+end)
 
-L:RegisterTranslations("esES", function() return {
-	--cmd = "Maexxna",
+L:RegisterTranslations("esES", function()
+    return {
+        --cmd = "Maexxna",
 
-	--spray_cmd = "spray",
-	spray_name = "Alerta de Pulverizador de tela de araña",
-	spray_desc = "Avisa para Pulverizador de tela de araña",
+        --spray_cmd = "spray",
+        spray_name = "Alerta de Pulverizador de tela de araña",
+        spray_desc = "Avisa para Pulverizador de tela de araña",
 
-	--enrage_cmd = "enrage",
-	enrage_name = "Alerta de Enfurecer",
-	enrage_desc = "Avisa para Enfurecer",
+        --enrage_cmd = "enrage",
+        enrage_name = "Alerta de Enfurecer",
+        enrage_desc = "Avisa para Enfurecer",
 
-	--cocoon_cmd = "cocoon",
-	cocoon_name = "Alerta de Capullo",
-	cocoon_desc = "Avisa para jugadores en Capullo",
+        --cocoon_cmd = "cocoon",
+        cocoon_name = "Alerta de Capullo",
+        cocoon_desc = "Avisa para jugadores en Capullo",
 
-	--poison_cmd = "Poison",
-	poison_name = "Alerta de Veneno necrótico",
-	poison_desc = "Avisa para Veneno necrótico",
+        --poison_cmd = "Poison",
+        poison_name = "Alerta de Veneno necrótico",
+        poison_desc = "Avisa para Veneno necrótico",
 
-	cocoontrigger = "(.*) (.*) sufre de Trampa arácnida.",
-	webspraytrigger = "sufre de Pulverizador de tela de araña",
-	poisontrigger = "sufre de Veneno necrótico.",
-	etrigger1 = "gana Enfurecer",
+        cocoontrigger = "(.*) (.*) sufre de Trampa arácnida.",
+        webspraytrigger = "sufre de Pulverizador de tela de araña",
+        poisontrigger = "sufre de Veneno necrótico.",
+        etrigger1 = "gana Enfurecer",
 
-	cocoonwarn = "¡%s en Capullo!",
-	poisonwarn = "¡Veneno necrótico!",
-	enragetrigger = "%s becomes enraged.",
+        cocoonwarn = "¡%s en Capullo!",
+        poisonwarn = "¡Veneno necrótico!",
+        enragetrigger = "%s becomes enraged.",
 
-	webspraywarn30sec = "Capullos al muro en 10 segundos",
-	webspraywarn20sec = "¡Capullos al muro! 15 segundos hasta aparezcan las arañas!",
-	webspraywarn10sec = "¡10 segundos hasta Pulverizador de tela de araña!",
-	webspraywarn5sec = "¡AOE - Aparecen las arañas - AOE! PULVERIZADOR DE TELA DE ARAÑA 5 SEGUNDOS!",
-	webspraywarn = "¡Pulverizador de tela de araña! 40 segundos hasta el próximo!",
+        webspraywarn30sec = "Capullos al muro en 10 segundos",
+        webspraywarn20sec = "¡Capullos al muro! 15 segundos hasta aparezcan las arañas!",
+        webspraywarn10sec = "¡10 segundos hasta Pulverizador de tela de araña!",
+        webspraywarn5sec = "¡AOE - Aparecen las arañas - AOE! PULVERIZADOR DE TELA DE ARAÑA 5 SEGUNDOS!",
+        webspraywarn = "¡Pulverizador de tela de araña! 40 segundos hasta el próximo!",
 
-	enragewarn = "¡Enfurecer!",
-	enragesoonwarn = "¡Enfurecer pronto!",
+        enragewarn = "¡Enfurecer!",
+        enragesoonwarn = "¡Enfurecer pronto!",
 
-	webspraybar = "Pulverizador de tela de araña",
-	cocoonbar = "Capullos",
-	spiderbar = "Arañás",
-	poisonbar = "Veneno necrótico",
+        webspraybar = "Pulverizador de tela de araña",
+        cocoonbar = "Capullos",
+        spiderbar = "Arañás",
+        poisonbar = "Veneno necrótico",
 
-	you = "Tu",
-	are = "estás",
-} end )
+        you = "Tu",
+        are = "estás",
+    }
+end)
 ---------------------------------
 --      	Variables 		   --
 ---------------------------------
@@ -109,30 +112,30 @@ L:RegisterTranslations("esES", function() return {
 module.revision = 20011 -- To be overridden by the module!
 module.enabletrigger = module.translatedName -- string or table {boss, add1, add2}
 --module.wipemobs = { L["add_name"] } -- adds which will be considered in CheckForEngage
-module.toggleoptions = {"spray", "poison", "cocoon", "enrage", "bosskill"}
+module.toggleoptions = { "spray", "poison", "cocoon", "enrage", "bosskill" }
 
 
 -- locals
 local timer = {
-	poison = {5, 10},
-	firstPoison = 15,
-	cocoon = 20,
-	spider = 30,
-	webspray = 40,
+    poison = { 5, 10 },
+    firstPoison = 15,
+    cocoon = 20,
+    spider = 30,
+    webspray = 40,
 }
 local icon = {
-	spider = "INV_Misc_MonsterSpiderCarapace_01",
-	cocoon = "Spell_Nature_Web",
-	poison = "Ability_Creature_Poison_03",
-	webspray = "Ability_Ensnare",
-	enrage = "Spell_shadow_unholyfrenzy",
+    spider = "INV_Misc_MonsterSpiderCarapace_01",
+    cocoon = "Spell_Nature_Web",
+    poison = "Ability_Creature_Poison_03",
+    webspray = "Ability_Ensnare",
+    enrage = "Spell_shadow_unholyfrenzy",
 }
 local syncName = {
-	webspray = "MaexxnaWebspray"..module.revision,
-	poison = "MaexxnaPoison"..module.revision,
-	cocoon = "MaexxnaCocoon"..module.revision,
-	enrage = "MaexxnaEnrage"..module.revision,
-	enragePercLeft = "MaexxnaEnragePercLeft"..module.revision,
+    webspray = "MaexxnaWebspray" .. module.revision,
+    poison = "MaexxnaPoison" .. module.revision,
+    cocoon = "MaexxnaCocoon" .. module.revision,
+    enrage = "MaexxnaEnrage" .. module.revision,
+    enragePercLeft = "MaexxnaEnragePercLeft" .. module.revision,
 }
 
 local times = {}
@@ -144,31 +147,36 @@ local enrageannounced = false
 
 -- called after module is enabled
 function module:OnEnable()
-	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_CREATURE_BUFFS", "Enrage")
-	--self:RegisterEvent("UNIT_HEALTH")
+    self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_CREATURE_BUFFS", "EnrageEvent")
+    self:RegisterEvent("UNIT_HEALTH", "HealthEvent")
 
-	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE", "SprayEvent")
-	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_DAMAGE", "SprayEvent")
-	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_PARTY_DAMAGE", "SprayEvent")
+    self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE", "SprayEvent")
+    self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_DAMAGE", "SprayEvent")
+    self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_PARTY_DAMAGE", "SprayEvent")
 
-	self:ThrottleSync(8, syncName.webspray)
-	self:ThrottleSync(5, syncName.poison)
-	self:ThrottleSync(0, syncName.cocoon)
-	-- the MaexxnaCocoon sync is left unthrottled, it's throttled inside the module itself
-	-- because the web wrap happens to a lot of players at once.
+    self:ThrottleSync(8, syncName.webspray)
+    self:ThrottleSync(5, syncName.poison)
+    self:ThrottleSync(0, syncName.cocoon)
+    -- the MaexxnaCocoon sync is left unthrottled, it's throttled inside the module itself
+    -- because the web wrap happens to a lot of players at once.
+    self:ThrottleSync(1, syncName.enrage)
+    self:ThrottleSync(1, syncName.enragePercLeft)
 end
 
 -- called after module is enabled and after each wipe
 function module:OnSetup()
-	enrageannounced = false
-	times = {}
+    enrageannounced = false
+    times = {}
 end
 
 -- called after boss is engaged
 function module:OnEngage()
-	self:KTM_SetTarget(self:ToString())
-	self:Bar(L["poisonbar"], timer.firstPoison, icon.poison)
-	self:Webspray()
+    self:KTM_SetTarget(self:ToString())
+    self:Bar(L["poisonbar"], timer.firstPoison, icon.poison)
+    self:Webspray()
+
+    self:TriggerEvent("BigWigs_StartHPBar", self, "% to Enrage", 100)
+    self:TriggerEvent("BigWigs_SetHPBar", self, "% to Enrage", 70)
 end
 
 -- called after boss is disengaged (wipe(retreat) or victory)
@@ -181,28 +189,37 @@ end
 ------------------------------
 
 function module:SprayEvent(msg)
-	-- web spray warning
-	if string.find(msg, L["webspraytrigger"]) then
-		self:Sync(syncName.webspray)
-	elseif string.find(msg, L["poisontrigger"]) then
-		self:Sync(syncName.poison)
-	elseif string.find(msg, L["cocoontrigger"]) then
-		local _,_,wplayer,wtype = string.find(msg, L["cocoontrigger"])
-		if wplayer and wtype then
-			if wplayer == L["you"] and wtype == L["are"] then
-				wplayer = UnitName("player")
-			end
-			local t = GetTime()
-			if (not times[wplayer]) or (times[wplayer] and (times[wplayer] + 10) < t) then
-				self:Sync(syncName.cocoon .. " " .. wplayer)
-			end
-		end
-	end
+    -- web spray warning
+    if string.find(msg, L["webspraytrigger"]) then
+        self:Sync(syncName.webspray)
+    elseif string.find(msg, L["poisontrigger"]) then
+        self:Sync(syncName.poison)
+    elseif string.find(msg, L["cocoontrigger"]) then
+        local _, _, wplayer, wtype = string.find(msg, L["cocoontrigger"])
+        if wplayer and wtype then
+            if wplayer == L["you"] and wtype == L["are"] then
+                wplayer = UnitName("player")
+            end
+            local t = GetTime()
+            if (not times[wplayer]) or (times[wplayer] and (times[wplayer] + 10) < t) then
+                self:Sync(syncName.cocoon .. " " .. wplayer)
+            end
+        end
+    end
 end
 
-function module:Enrage(msg)
-	DEFAULT_CHAT_FRAME:AddMessage(msg)
-	--self:Sync(syncName.poison)
+function module:EnrageEvent(msg)
+    self:Sync(syncName.enrage)
+end
+
+function module:HealthEvent(msg)
+
+    if UnitExists('target') then
+        if UnitName('target') == 'Maexxna' and msg == 'target' then
+            self:Sync(syncName.enragePercLeft .. " " .. 100 + 30 - UnitHealth('target'))
+        end
+    end
+
 end
 
 ------------------------------
@@ -210,13 +227,17 @@ end
 ------------------------------
 
 function module:BigWigs_RecvSync(sync, rest)
-	if sync == syncName.webspray then
-		self:Webspray()
-	elseif sync == syncName.poison then
-		self:Poison()
-	elseif sync == syncName.cocoon and rest then
-		self:Cocoon(rest)
-	end
+    if sync == syncName.webspray then
+        self:Webspray()
+    elseif sync == syncName.poison then
+        self:Poison()
+    elseif sync == syncName.cocoon and rest then
+        self:Cocoon(rest)
+    elseif sync == syncName.enrage then
+        self:Enrage()
+    elseif sync == syncName.enragePercLeft and rest then
+        self:Health(tonumber(rest))
+    end
 end
 
 
@@ -225,35 +246,51 @@ end
 ------------------------------
 
 function module:Webspray()
-	--self:CancelDelayedMessage(L["webspraywarn30sec"])
-	--self:CancelDelayedMessage(L["webspraywarn20sec"])
-	--self:CancelDelayedMessage(L["webspraywarn10sec"])
-	--self:CancelDelayedMessage(L["webspraywarn5sec"])
+    --self:CancelDelayedMessage(L["webspraywarn30sec"])
+    --self:CancelDelayedMessage(L["webspraywarn20sec"])
+    --self:CancelDelayedMessage(L["webspraywarn10sec"])
+    --self:CancelDelayedMessage(L["webspraywarn5sec"])
 
-	self:Message(L["webspraywarn"], "Important")
-	self:Bar(L["cocoonbar"], timer.cocoon, icon.cocoon)
-	self:Bar(L["spiderbar"], timer.spider, icon.spider)
-	self:Bar(L["webspraybar"], timer.webspray, icon.webspray)
+    self:Message(L["webspraywarn"], "Important")
+    self:Bar(L["cocoonbar"], timer.cocoon, icon.cocoon)
+    self:Bar(L["spiderbar"], timer.spider, icon.spider)
+    self:Bar(L["webspraybar"], timer.webspray, icon.webspray)
 
-	--self:DelayedMessage(timer.webspray - 30, L["webspraywarn30sec"], "Attention")
-	--self:DelayedMessage(timer.webspray - 20, L["webspraywarn20sec"], "Attention")
-	--self:DelayedMessage(timer.webspray - 10, L["webspraywarn10sec"], "Attention")
-	--self:DelayedMessage(timer.webspray - 5, L["webspraywarn5sec"], "Attention")
+    --self:DelayedMessage(timer.webspray - 30, L["webspraywarn30sec"], "Attention")
+    --self:DelayedMessage(timer.webspray - 20, L["webspraywarn20sec"], "Attention")
+    --self:DelayedMessage(timer.webspray - 10, L["webspraywarn10sec"], "Attention")
+    --self:DelayedMessage(timer.webspray - 5, L["webspraywarn5sec"], "Attention")
 end
 
 function module:Poison()
-	if self.db.profile.poison then
-		self:Message(L["poisonwarn"], "Important")
-		self:IntervalBar(L["poisonbar"], timer.poison[1], timer.poison[2], icon.poison)
-	end
+    if self.db.profile.poison then
+        self:Message(L["poisonwarn"], "Important")
+        self:IntervalBar(L["poisonbar"], timer.poison[1], timer.poison[2], icon.poison)
+    end
 end
 
 function module:Cocoon(player)
-	local t = GetTime()
-	if (not times[player]) or (times[player] and (times[player] + 10) < t) then
-		if self.db.profile.cocoon then
-			self:Message(string.format(L["cocoonwarn"], player), "Urgent")
-		end
-		times[player] = t
-	end
+    local t = GetTime()
+    if (not times[player]) or (times[player] and (times[player] + 10) < t) then
+        if self.db.profile.cocoon then
+            self:Message(string.format(L["cocoonwarn"], player), "Urgent")
+        end
+        times[player] = t
+    end
+end
+
+function module:Enrage()
+    self:Message("Maexxna becomes Enraged !", "Important")
+end
+
+function module:Health(perc)
+
+    self:TriggerEvent("BigWigs_SetHPBar", self, "% to Enrage", perc)
+
+    if perc == 40 then
+        self:Message("Enrage in 10% !", "Important")
+    end
+    if perc == 35 then
+        self:Message("Enrage in 5% !", "Important")
+    end
 end
