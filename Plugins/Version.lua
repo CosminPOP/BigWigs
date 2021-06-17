aa2ea22b20ecb27786911df1c5cd1dc570809d94
Cosmin POP <mtx.rake@gmail.com>
2021-06-17 20:28:09 +0300

    version

--- Plugins/Version.lua
+++ Plugins/Version.lua
@@ -43,7 +43,7 @@ L:RegisterTranslations("enUS", function() return {
 	["Nr Replies"] = true,
 	["Ancient"] = true,
 
-	["Your version of Big Wigs Turtle-WoW is out of date!\nPlease visit https://github.com/Hosq/BigWigs to get the latest version."] = true,
+	["Your version of Big Wigs Turtle-WoW is out of date!\nPlease visit https://github.com/CosminPOP/BigWigs to get the latest version."] = true,
 	["Close"] = true,
 	["Cancel"] = true,
 
@@ -51,7 +51,7 @@ L:RegisterTranslations("enUS", function() return {
 	["Notify old versions"] = true,
 	["Notify old versions"] = true,
 	["List people with old versions to raid chat."] = true,
-	["Download newest version from https://github.com/Hosq/BigWigs"] = true,
+	["Download newest version from hhttps://github.com/CosminPOP/BigWigs"] = true,
 
 	["Show popup"] = true,
 	["Show popup warning on out of date version"] = true,
@@ -85,14 +85,14 @@ L:RegisterTranslations("esES", function() return {
 	["Nr Replies"] = "Nr repuesta",
 	["Ancient"] = "Anciano",
 
-	["Your version of Big Wigs Turtle-WoW is out of date!\nPlease visit https://github.com/Hosq/BigWigs to get the latest version."] = "¡Tu versión de Big Wigs es anticuada!\n Por favor visite https://github.com/Hosq/BigWigs para descargar la última versión.",
+	["Your version of Big Wigs Turtle-WoW is out of date!\nPlease visit https://github.com/CosminPOP/BigWigs to get the latest version."] = "¡Tu versión de Big Wigs es anticuada!\n Por favor visite https://github.com/CosminPOP/BigWigs para descargar la última versión.",
 	["Close"] = "Cerrar",
 	["Cancel"] = "Cancelar",
 
 	["People with outdated BigWigs:"] = "Personas con BigWigs anticuado:",
 	["Notify old versions"] = "Notificar de versiones anticuadas",
 	["List people with old versions to raid chat."] = "Enumera las personas con versiones anticuadas al chat de la banda",
-	["Download newest version from https://github.com/Hosq/BigWigs"] = "Descarga la última versión en https://github.com/Hosq/BigWigs",
+	["Download newest version from https://github.com/CosminPOP/BigWigs"] = "Descarga la última versión en https://github.com/CosminPOP/BigWigs",
 
 	["Show popup"] = "Mostrar Ventana Emergente",
 	["Show popup warning on out of date version"] = "Muesta la ventana emergente para versiones anticuadas",
@@ -126,7 +126,7 @@ L:RegisterTranslations("deDE", function() return {
 	["Nr Replies"] = "Anzahl der Antworten",
 	["Ancient"] = "Alt",
 
-	["Your version of Big Wigs Turtle-WoW is out of date!\nPlease visit https://github.com/Hosq/BigWigs to get the latest version."] = "Deine Version von Big Wigs Turtle-WoW ist veraltet! Bitte downloade die neuste Version von https://github.com/Hosq/BigWigs",
+	["Your version of Big Wigs Turtle-WoW is out of date!\nPlease visit https://github.com/CosminPOP/BigWigs to get the latest version."] = "Deine Version von Big Wigs Turtle-WoW ist veraltet! Bitte downloade die neuste Version von https://github.com/CosminPOP/BigWigs",
 	["Close"] = "Schliessen",
 	["Cancel"] = "Abbrechen",
 } end )
@@ -293,12 +293,12 @@ end
 function BigWigsVersionQuery:IsOutOfDate()
 	if not self.OutOfDateShown then
 		self.OutOfDateShown = true
-		BigWigs:Print(L["Your version of Big Wigs Turtle-WoW is out of date!\nPlease visit https://github.com/Hosq/BigWigs to get the latest version."])
+		BigWigs:Print(L["Your version of Big Wigs Turtle-WoW is out of date!\nPlease visit https://github.com/CosminPOP/BigWigs to get the latest version."])
 
 		if self.db.profile.popup then
 			local dialog = nil
 			StaticPopupDialogs["BigWigsOutOfDateDialog"] = {
-				text = L["Your version of Big Wigs Turtle-WoW is out of date!\nPlease visit https://github.com/Hosq/BigWigs to get the latest version."],
+				text = L["Your version of Big Wigs Turtle-WoW is out of date!\nPlease visit https://github.com/CosminPOP/BigWigs to get the latest version."],
 				button1 = L["Close"],
 				button2 = L["Cancel"],
 				OnAccept = function()
@@ -309,7 +309,7 @@ function BigWigsVersionQuery:IsOutOfDate()
 				end,
 				OnShow = function (self, data)
 					local editbox = getglobal(this:GetName().."WideEditBox")
-					editbox:SetText("https://github.com/Hosq/BigWigs")
+					editbox:SetText("https://github.com/CosminPOP/BigWigs")
 					editbox:SetWidth(250)
 					editbox:ClearFocus()
 					editbox:HighlightText()
@@ -343,7 +343,7 @@ function BigWigsVersionQuery:NotifyOldVersions()
 	end
 	SendChatMessage(L["People with outdated BigWigs:"],"RAID")
 	SendChatMessage(line,"RAID")
-	SendChatMessage(L["Download newest version from https://github.com/Hosq/BigWigs"], "RAID")
+	SendChatMessage(L["Download newest version from https://github.com/CosminPOP/BigWigs"], "RAID")
 end
 
 function BigWigsVersionQuery:OnTooltipUpdate()
