b8681e8acf462dbca3dae5ed3d374b8b3085c773
Cosmin POP <mtx.rake@gmail.com>
2021-09-20 13:19:19 +0300

    revamped void zone logic

--- Raids/Naxxramas/Horsemen.lua
+++ Raids/Naxxramas/Horsemen.lua
@@ -129,7 +129,6 @@ end)
 -- module variables
 module.revision = 20005 -- To be overridden by the module!
 module.enabletrigger = { thane, mograine, zeliek, blaumeux } -- string or table {boss, add1, add2}
---module.wipemobs = { L["add_name"] } -- adds which will be considered in CheckForEngage
 module.toggleoptions = { "mark", "shieldwall", -1, "meteor", "void", "wrath", "bosskill" }
 
 
@@ -155,10 +154,12 @@ local icon = {
 local syncName = {
     shieldwall = "HorsemenShieldWall" .. module.revision,
     mark = "HorsemenMark" .. module.revision,
-    void = "HorsemenVoid" .. module.revision,
+    DelayedVoidZone = "HorsemenDelayedVoidZone" .. module.revision,
     wrath = "HorsemenWrath" .. module.revision,
     meteor = "HorsemenMeteor" .. module.revision,
 }
+
+local void_trigger = "Lady Blaumeux casts Void Zone"
 
 local times = nil
 local globalMarks = 0
@@ -182,7 +183,7 @@ function module:OnEnable()
 
     self:ThrottleSync(3, syncName.shieldwall)
     self:ThrottleSync(8, syncName.mark)
-    self:ThrottleSync(5, syncName.void)
+    self:ThrottleSync(5, syncName.DelayedVoidZone)
     self:ThrottleSync(5, syncName.wrath)
     self:ThrottleSync(5, syncName.meteor)
 end
@@ -236,7 +237,6 @@ fhAlert:SetScript("OnEvent", function()
                         break
                     end
                 end
-
             end
         end
     end
@@ -321,6 +321,47 @@ function module:MarkEvent(msg)
     end
 end
 
+function module:DelayedVoidZone(msg)
+    if string.find(msg, void_trigger) then
+        self:Sync(syncName.DelayedVoidZone)
+    end
+end
+
+function module:VoidZoneEvent()
+    self:ScheduleEvent("DelayedVoidZoneEvent", self.DelayedVoidZoneEvent, 0.2, self)
+    self:WarningSign(icon.void, 3)
+    self:IntervalBar(L["voidbar"], timer.void[1], timer.void[2], icon.void)
+end
+
+function module:DelayedVoidZoneEvent()
+    local target = self:CheckTarget()
+
+    if target then
+        self:Icon(target, 3)
+        if target == UnitName("player") then
+            self:Message("Void Zone on YOU !!!", "Important")
+            self:TriggerEvent("BigWigs_Sound", "AirHorn")
+            SendChatMessage("Void Zone On Me !", "SAY")
+        else
+            self:Message("Void Zone on " .. target .. " !!!", "Important")
+        end
+    end
+end
+
+function module:CheckTarget()
+
+    if UnitName("target") == blaumeux then
+        return UnitName("targettarget")
+    else
+        for i = 1, GetNumRaidMembers() do
+            if UnitName("Raid" .. i .. "target") == blaumeux then
+                return UnitName("Raid" .. i .. "targettarget")
+            end
+        end
+    end
+    return nil
+end
+
 function module:CHAT_MSG_SPELL_PERIODIC_CREATURE_BUFFS(msg)
     local _, _, mob = string.find(msg, L["shieldwalltrigger"])
     if mob then
@@ -330,7 +371,7 @@ end
 
 function module:CHAT_MSG_MONSTER_SAY(msg)
     if string.find(msg, L["voidtrigger"]) then
-        self:Sync(syncName.void)
+        self:Sync(syncName.DelayedVoidZone)
     elseif string.find(msg, L["meteortrigger2"]) then
         self:Sync(syncName.meteor)
     elseif string.find(msg, L["wrathtrigger2"]) then
@@ -363,8 +404,8 @@ function module:BigWigs_RecvSync(sync, rest, nick)
         self:Meteor()
     elseif sync == syncName.wrath then
         self:Wrath()
-    elseif sync == syncName.void then
-        self:Void()
+    elseif sync == syncName.DelayedVoidZone then
+        self:VoidZoneEvent()
     elseif sync == syncName.shieldwall and rest then
         self:Shieldwall(rest)
     end
@@ -496,17 +537,6 @@ function module:Wrath()
     end
 end
 
-function module:Void()
-    if self.db.profile.void then
-        self:Message(L["voidwarn"], "Important")
-        self:IntervalBar(L["voidbar"], timer.void[1], timer.void[2], icon.void)
-        if (UnitExists('target') and UnitName('target') == blaumeux) or
-                (UnitExists('targettarget') and UnitName('targettarget') == blaumeux) then
-            self:WarningSign(icon.void, 3)
-        end
-    end
-end
-
 function module:Shieldwall(mob)
     if mob and self.db.profile.shieldwall then
         self:Message(string.format(L["shieldwall_warn"], mob), "Attention")
@@ -514,46 +544,6 @@ function module:Shieldwall(mob)
         self:DelayedMessage(timer.shieldwall, string.format(L["shieldwall_warn_over"], mob), "Positive")
     end
 end
-
---[[
-1608234547688 1608234547.6880 Thu Dec 17 21:49:08 UTC 2020 Sir Zeliek Holy Wrath
-1608234562266 1608234562.2660 Thu Dec 17 21:49:22 UTC 2020 Sir Zeliek Holy Wrath 14
-1608234575203 1608234575.2030 Thu Dec 17 21:49:35 UTC 2020 Sir Zeliek Holy Wrath 13
-1608234589813 1608234589.8130 Thu Dec 17 21:49:50 UTC 2020 Sir Zeliek Holy Wrath 15
-1608234602781 1608234602.7810 Thu Dec 17 21:50:03 UTC 2020 Sir Zeliek Holy Wrath 13
-1608234614125 1608234614.1250 Thu Dec 17 21:50:14 UTC 2020 Sir Zeliek Holy Wrath 11
-1608234630313 1608234630.3130 Thu Dec 17 21:50:30 UTC 2020 Sir Zeliek Holy Wrath 16
-1608234644875 1608234644.8750 Thu Dec 17 21:50:45 UTC 2020 Sir Zeliek Holy Wrath 15
-1608234657844 1608234657.8440 Thu Dec 17 21:50:58 UTC 2020 Sir Zeliek Holy Wrath 13
-1608234672391 1608234672.3910 Thu Dec 17 21:51:12 UTC 2020 Sir Zeliek Holy Wrath 14
-1608234685328 1608234685.3280 Thu Dec 17 21:51:25 UTC 2020 Sir Zeliek Holy Wrath 13
-1608234697110 1608234697.1100 Thu Dec 17 21:51:37 UTC 2020 Sir Zeliek Holy Wrath 12
-1608234709625 1608234709.6250 Thu Dec 17 21:51:50 UTC 2020 Sir Zeliek Holy Wrath 13
-1608234720969 1608234720.9690 Thu Dec 17 21:52:01 UTC 2020 Sir Zeliek Holy Wrath 11
-1608234733891 1608234733.8910 Thu Dec 17 21:52:14 UTC 2020 Sir Zeliek Holy Wrath 13
-1608234745266 1608234745.2660 Thu Dec 17 21:52:25 UTC 2020 Sir Zeliek Holy Wrath 11
-1608234756578 1608234756.5780 Thu Dec 17 21:52:37 UTC 2020 Sir Zeliek Holy Wrath 12
-1608234771141 1608234771.1410 Thu Dec 17 21:52:51 UTC 2020 Sir Zeliek Holy Wrath 14
-1608234784110 1608234784.1100 Thu Dec 17 21:53:04 UTC 2020 Sir Zeliek Holy Wrath 13
-1608234797078 1608234797.0780 Thu Dec 17 21:53:17 UTC 2020 Sir Zeliek Holy Wrath 13
-1608234811656 1608234811.6560 Thu Dec 17 21:53:32 UTC 2020 Sir Zeliek Holy Wrath 15
-1608234824625 1608234824.6250 Thu Dec 17 21:53:45 UTC 2020 Sir Zeliek Holy Wrath 13
-1608234837625 1608234837.6250 Thu Dec 17 21:53:58 UTC 2020 Sir Zeliek Holy Wrath 13
-1608234852219 1608234852.2190 Thu Dec 17 21:54:12 UTC 2020 Sir Zeliek Holy Wrath 14
-1608234865172 1608234865.1720 Thu Dec 17 21:54:25 UTC 2020 Sir Zeliek Holy Wrath 13
-1608234878125 1608234878.1250 Thu Dec 17 21:54:38 UTC 2020 Sir Zeliek Holy Wrath 13
-1608234891078 1608234891.0780 Thu Dec 17 21:54:51 UTC 2020 Sir Zeliek Holy Wrath 13
-1608234902406 1608234902.4060 Thu Dec 17 21:55:02 UTC 2020 Sir Zeliek Holy Wrath 11
-1608234918641 1608234918.6410 Thu Dec 17 21:55:19 UTC 2020 Sir Zeliek Holy Wrath
-1608234933219 1608234933.2190 Thu Dec 17 21:55:33 UTC 2020 Sir Zeliek Holy Wrath
-1608234944578 1608234944.5780 Thu Dec 17 21:55:45 UTC 2020 Sir Zeliek Holy Wrath
-1608234957500 1608234957.5000 Thu Dec 17 21:55:58 UTC 2020 Sir Zeliek Holy Wrath
-1608234972047 1608234972.0470 Thu Dec 17 21:56:12 UTC 2020 Sir Zeliek Holy Wrath
-1608234984969 1608234984.9690 Thu Dec 17 21:56:25 UTC 2020 Sir Zeliek Holy Wrath
-1608234996281 1608234996.2810 Thu Dec 17 21:56:36 UTC 2020 Sir Zeliek Holy Wrath
-
-]]--
-
 
 function string:split(delimiter)
     local result = {}
@@ -590,82 +580,82 @@ function module:Test()
 
     --mark2
     time = time + timer.firstMark -- 20
-    self:ScheduleEvent(self:ToString().."Test_mark2", mark, time, self)
+    self:ScheduleEvent(self:ToString() .. "Test_mark2", mark, time, self)
     BigWigs:Print("module Test schedule mark(2) @ " .. time)
 
     --mark3
     time = time + timer.mark -- 32
-    self:ScheduleEvent(self:ToString().."Test_mark3", mark, time, self)
+    self:ScheduleEvent(self:ToString() .. "Test_mark3", mark, time, self)
     BigWigs:Print("module Test schedule mark(3) @ " .. time)
 
     --mark4
     time = time + timer.mark -- 44
-    self:ScheduleEvent(self:ToString().."Test_mark4", mark, time, self)
+    self:ScheduleEvent(self:ToString() .. "Test_mark4", mark, time, self)
     BigWigs:Print("module Test schedule mark(4) @ " .. time)
 
     --mark5
     time = time + timer.mark --56
-    self:ScheduleEvent(self:ToString().."Test_mark5", mark, time, self)
+    self:ScheduleEvent(self:ToString() .. "Test_mark5", mark, time, self)
     BigWigs:Print("module Test schedule mark(5) @ " .. time)
 
     --mark6
     time = time + timer.mark -- 68
-    self:ScheduleEvent(self:ToString().."Test_mark6", mark, time, self)
+    self:ScheduleEvent(self:ToString() .. "Test_mark6", mark, time, self)
     BigWigs:Print("module Test schedule mark(6) @ " .. time)
 
     --mark7
     time = time + timer.mark -- 80
-    self:ScheduleEvent(self:ToString().."Test_mark7", mark, time, self)
+    self:ScheduleEvent(self:ToString() .. "Test_mark7", mark, time, self)
     BigWigs:Print("module Test schedule mark(7) @ " .. time)
 
     --mark8
     time = time + timer.mark -- 92
-    self:ScheduleEvent(self:ToString().."Test_mark8", mark, time, self)
+    self:ScheduleEvent(self:ToString() .. "Test_mark8", mark, time, self)
     BigWigs:Print("module Test schedule mark(8) @ " .. time)
 
     --mark9
     time = time + timer.mark -- 104
-    self:ScheduleEvent(self:ToString().."Test_mark9", mark, time, self)
+    self:ScheduleEvent(self:ToString() .. "Test_mark9", mark, time, self)
     BigWigs:Print("module Test schedule mark(9) @ " .. time)
 
     --mark10
     time = time + timer.mark -- 116
-    self:ScheduleEvent(self:ToString().."Test_mark10", mark, time, self)
+    self:ScheduleEvent(self:ToString() .. "Test_mark10", mark, time, self)
     BigWigs:Print("module Test schedule mark(10) @ " .. time)
 
     --mark11
     time = time + timer.mark -- 128
-    self:ScheduleEvent(self:ToString().."Test_mark11", mark, time, self)
+    self:ScheduleEvent(self:ToString() .. "Test_mark11", mark, time, self)
     BigWigs:Print("module Test schedule mark(11) @ " .. time)
 
     --mark12
     time = time + timer.mark -- 140
-    self:ScheduleEvent(self:ToString().."Test_mark12", mark, time, self)
+    self:ScheduleEvent(self:ToString() .. "Test_mark12", mark, time, self)
     BigWigs:Print("module Test schedule mark(12) @ " .. time)
 
     --mark13
     time = time + timer.mark -- 152
-    self:ScheduleEvent(self:ToString().."Test_mark13", mark, time, self)
+    self:ScheduleEvent(self:ToString() .. "Test_mark13", mark, time, self)
     BigWigs:Print("module Test schedule mark(13) @ " .. time)
 
     --mark14
     time = time + timer.mark -- 164
-    self:ScheduleEvent(self:ToString().."Test_mark14", mark, time, self)
+    self:ScheduleEvent(self:ToString() .. "Test_mark14", mark, time, self)
     BigWigs:Print("module Test schedule mark(14) @ " .. time)
 
     --mark15
     time = time + timer.mark -- 176
-    self:ScheduleEvent(self:ToString().."Test_mark15", mark, time, self)
+    self:ScheduleEvent(self:ToString() .. "Test_mark15", mark, time, self)
     BigWigs:Print("module Test schedule mark(15) @ " .. time)
 
     --mark16
     time = time + timer.mark -- 188
-    self:ScheduleEvent(self:ToString().."Test_mark16", mark, time, self)
+    self:ScheduleEvent(self:ToString() .. "Test_mark16", mark, time, self)
     BigWigs:Print("module Test schedule mark(16) @ " .. time)
 
 
     -- reset after 4m
     time = 240
     BigWigs:Print(" deactivate after " .. time)
-    self:ScheduleEvent(self:ToString().."Test_deactivate", deactivate, time, self)
+    self:ScheduleEvent(self:ToString() .. "Test_deactivate", deactivate, time, self)
 end
